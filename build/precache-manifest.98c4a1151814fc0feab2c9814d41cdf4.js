c031560f2e6786f3a063547fbb9f669b247217e6
Ashok reddy <ashokcse505@gmail.com>
2019-05-21 22:58:07 +0530

    added

---
+++ build/precache-manifest.98c4a1151814fc0feab2c9814d41cdf4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c5590569aab553af03722baa7d379978",
+    "url": "/static/media/loading.c5590569.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "51b8730305a8335f3777",
+    "url": "/static/js/main.01e9ea4e.chunk.js"
+  },
+  {
+    "revision": "82166df11ae97eca3d16",
+    "url": "/static/js/2.a09c80fa.chunk.js"
+  },
+  {
+    "revision": "50b6a8925c505ea68cbad66a043f5098",
+    "url": "/index.html"
+  }
+];
