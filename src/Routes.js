bb8d4933e00b90e2ac7acbae33194d2afc8b9b1b
srinivas99 <nalluri99489@gmail.com>
2019-05-24 20:03:49 +0530

    register adding

--- src/Routes.js
+++ src/Routes.js
@@ -4,7 +4,8 @@ import App from './App';
 import Callback from './Callback/Callback';
 import Auth from './Auth/Auth';
 import history from './history';
-import Prices from "./Pages/Prices"
+import Prices from "./Pages/Prices";
+import Register from "./Pages/Register";
 
 const auth = new Auth();
 
@@ -21,7 +22,7 @@ const Routes = () => (
       <Router history={history}>
         <div>
           <Route path="/" render={(props) => <App auth={auth} {...props} />} />
-          {/* <Route path="/home"  component={Prices} /> */}
+          <Route path="/Register"  component={Register} />
           <Route path="/home" render={(props) => <Prices auth={auth} {...props} />} />
           <Route path="/callback" render={(props) => {
             handleAuthentication(props);
